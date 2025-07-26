a0ba68b054fc2a789fb54d683cfec56731be78ac
arielmp11 <arielmorais644@gmail.com>
2025-07-25 23:25:02 -0300

    feat:Adiciona Estrutura de Pastas
    Adicionando estrutura de pastas para organização do projeto

---
+++ Models/Materias.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora_de_Notas_POO.Models
+{
+    public class Materias
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Professor { get; set; }
+        public int Periodo { get; set; }
+    }
+}
---
+++ Repositories/MateriaRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora_de_Notas_POO.Repositories
+{
+    internal class MateriaRepository
+    {
+    }
+}
