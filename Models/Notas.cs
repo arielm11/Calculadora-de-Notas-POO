a261c31e2652b3fd82903c2e4a76fbab30175ce6
arielmp11 <arielmorais644@gmail.com>
2025-08-11 22:16:37 -0300

    feat(models): Add Atributos
    Inserindo os atributos da classe para conseguir trabalhar nela futuramente.

--- Models/Notas.cs
+++ Models/Notas.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Calculadora_de_Notas_POO.Models
 {
-    internal class Notas
+    public class Notas
     {
+        public int Id { get; set; } 
+        public int IdMateria { get; set; } 
+        public decimal PrimeiraNota { get; set; } 
+        public decimal SegundaNota { get; set; } 
+        public decimal ExameFinal { get; set; } 
+        public decimal NotaFinal { get; set; }
     }
 }
